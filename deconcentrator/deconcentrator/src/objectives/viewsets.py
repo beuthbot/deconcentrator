72ce5461b9d2b26109dd8586a94571611dc89e24
Kai Nessig <gitkraken@dasgibtsdochso.net>
2019-12-29 16:07:46 +0100

    permissions on viewsets

--- deconcentrator/deconcentrator/src/objectives/viewsets.py
+++ deconcentrator/deconcentrator/src/objectives/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .models import *
 from .serializers import *
 
@@ -6,18 +7,22 @@ from .serializers import *
 class StrategyViewSet(viewsets.ModelViewSet):
     queryset = Strategy.objects.all()
     serializer_class = StrategySerializer
+    permission_classes = [AllowAny]
 
 
 class ObjectiveViewSet(viewsets.ModelViewSet):
     queryset = Objective.objects.all()
     serializer_class = ObjectiveSerializer
+    permission_classes = [AllowAny]
 
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    permission_classes = [AllowAny]
 
 
 class ResultViewSet(viewsets.ModelViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
+    permission_classes = [AllowAny]
--- deconcentrator/deconcentrator/src/providers/viewsets.py
+++ deconcentrator/deconcentrator/src/providers/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .models import *
 from .serializers import *
 
@@ -6,9 +7,11 @@ from .serializers import *
 class MethodViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Method.objects.all()
     serializer_class = MethodSerializer
+    permission_classes = [AllowAny]
 
 
 class ProviderViewSet(viewsets.ModelViewSet):
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
+    permission_classes = [AllowAny]
 

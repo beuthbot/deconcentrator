b37d588311c3e49d1ce51c479f8bb8c444659d67
Kai Nessig <gitkraken@dasgibtsdochso.net>
2020-01-23 20:37:16 +0100

    allow sync mode of operation

--- deconcentrator/src/objectives/methods.py
+++ deconcentrator/src/objectives/methods.py
@@ -3,10 +3,22 @@ from objectives.tasks import evaluate_task, callback_task
 
 def evaluate(job):
     """ evaluate the given message (voice or text) by transmitting to the NLU provider. """
-    evaluate_task.delay(job.pk)
+    if job.objective.kwargs.get('sync', False):
+        method = evaluate_task
+
+    else:
+        method = evaluate_task.delay
+
+    method(job.pk)
 
 
 def callback(job):
     """ not a method by definition, but: do a callback if there's an endpoint. """
     if "callback" in job.objective.kwargs:
-        callback_task.delay(job.pk)
+        if job.objective.kwargs.get('sync', False):
+            method = callback_task
+
+        else:
+            method = callback_task.delay
+
+        method(job.pk)

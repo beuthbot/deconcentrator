5f2e88c71193f496f1a8210a42f084b5d4381e95
Kai Nessig <gitkraken@dasgibtsdochso.net>
2020-01-23 22:34:01 +0100

    `dispatch_uid`s against multiple handler executions

--- deconcentrator/src/objectives/apps.py
+++ deconcentrator/src/objectives/apps.py
@@ -7,10 +7,10 @@ class AppConfig(DjangoAppConfig):
 
     def ready(self):
         from .handlers import post_migrate_hook
-        post_migrate.connect(post_migrate_hook)
+        post_migrate.connect(post_migrate_hook, dispatch_uid="jighohmodief9Uch")
 
         from .handlers import post_save_hook
         from .models import Objective, Job, Result
-        post_save.connect(post_save_hook, sender=Objective)
-        post_save.connect(post_save_hook, sender=Job)
-        post_save.connect(post_save_hook, sender=Result)
+        post_save.connect(post_save_hook, sender=Objective, dispatch_uid="shie4Au4mo7ohgh9#maketolgagreatagain")
+        post_save.connect(post_save_hook, sender=Job, dispatch_uid="Ceecighiishee7da#maketolgagreatagain")
+        post_save.connect(post_save_hook, sender=Result, dispatch_uid="queemep9yanie3Ro#maketolgagreatagain")

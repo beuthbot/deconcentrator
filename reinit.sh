7709566d9a95f754e6cd5e3f353ec8fe29bc3928
Kai Nessig <gitkraken@dasgibtsdochso.net>
2020-01-23 23:12:38 +0100

    enhance migration handling

--- reinit.sh
+++ reinit.sh
@@ -2,8 +2,9 @@
 
 bash down -v &&
 bash prepare &&
+find deconcentrator -type d -name migrations -exec find '{}' -type f -print0 \; | xargs -0 rm -v 
+find deconcentrator -type d -name migrations -print0 | xargs -0 git checkout -- 
 bash up --build -d --scale=worker=3 &&
-rm -vf deconcentrator/src/*/migrations/0*.py &&
 bash management.sh exec uwsgi /mnt/deconcentrator/manage.py makemigrations &&
 bash management.sh exec uwsgi /mnt/deconcentrator/manage.py migrate &&
 bash management.sh exec uwsgi /mnt/deconcentrator/manage.py collectstatic --noinput &&
